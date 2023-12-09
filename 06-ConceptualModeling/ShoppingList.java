b2249c089facf6d1058f275c917b94f556b2330e
Darya <225284@student.uek.krakow.pl>
2023-12-10 00:59:30 +0100

    10.12.2023

--- 06-ConceptualModeling/ShoppingList.java
+++ 06-ConceptualModeling/ShoppingList.java
@@ -1,39 +1,36 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class ShoppingList {
-    ArrayList<String> products = new ArrayList<>();
+public class ShoppingList{
+   ArrayList<String> products = new ArrayList<>();
 
-    public void display(){
-        System.out.println(products);
-    }
+   void display(){
+      System.out.println(products);
+   }
 
-    public void display_number(){
-        System.out.println(products.size());
-    }
+   void NumProducts(){
+      System.out.println(products.size());
+   }
 
-    public void add_product(String product){
-        products.add(product);
-    }
+   void AddProduct(String product){
+      products.add(product);
+   }
+   
 
 
 
-    public static void main(String[] args) {
-    ShoppingList a = new ShoppingList();
-    Scanner keyboard = new Scanner(System.in);
-    a.display();    
-    a.display_number();
-    String product = keyboard.nextLine();
-    a.add_product(product);
-        a.display();
-    String product1 = keyboard.nextLine();
-    a.add_product(product1);
-        a.display();
-    String product2 = keyboard.nextLine();
-    a.add_product(product2);
-    a.display();
-    a.display_number();
-    a.display();
-    a.display_number();
-    }
+
+   public static void main(String[] args) {
+      ShoppingList c = new ShoppingList();
+      Scanner a = new Scanner(System.in);
+      String product = a.nextLine();
+      c.AddProduct(product);
+      c.display();
+      c.NumProducts();
+      c.AddProduct("kiwi");
+      c.display();
+      c.NumProducts();
+
+   }
+
 }
